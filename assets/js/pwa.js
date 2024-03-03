500aca0bdb4a2d77889e4b542e288b7542cd8f85
Kashyap Patel <kashoopatel80@gmail.comclear>
2024-03-03 12:01:33 +0530

    prompt change

--- assets/js/pwa.js
+++ assets/js/pwa.js
@@ -5,15 +5,28 @@
 // Check compatibility for the browser we're running this in
 if ("serviceWorker" in navigator) {
   if (navigator.serviceWorker.controller) {
-    console.log("[PWA Builder] active service worker found, no need to register");
+    console.log(
+      "[PWA Builder] active service worker found, no need to register"
+    );
   } else {
     // Register the service worker
     navigator.serviceWorker
       .register("pwabuilder-sw.js", {
-        scope: "./"
+        scope: "./",
       })
-      .then(function(reg) {
-        console.log("[PWA Builder] Service worker has been registered for scope: " + reg.scope);
+      .then(function (reg) {
+        console.log(
+          "[PWA Builder] Service worker has been registered for scope: " +
+            reg.scope
+        );
       });
   }
 }
+
+let installPrompt;
+window.addEventListener("beforeinstallprompt", (event) => {
+  // Prevent the default browser install prompt
+  event.preventDefault();
+  installPrompt = event;
+  console.log(first);
+});
